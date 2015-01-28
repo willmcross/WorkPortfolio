9a5369b187ef5e16f6c68af27efa8f833ceac94b
James Spinella <james.spinella@jatsby.com>
2015-01-28 18:23:41 -0500

    Laid out Collection class
    Added all methods for Collection class.

--- Project 1/src/Collection.java
+++ Project 1/src/Collection.java
@@ -1,4 +1,77 @@
 
 public class Collection {
 
+  // Declare n-element Array
+  // private int[] array = new int[]{};
+
+  // Adds a Student object to the collection
+  public string add() {
+
+  }
+
+  // Searches the collection using the Binary Search algorithm
+  public int binarySearch()  {
+
+  }
+
+  // Sorts the collection on a student’s full name using the Bubble Sort algorithm
+  public int bubbleSort()  {
+
+  }
+
+  // Displays the student object data stored in the collection
+  public double display() {
+
+  }
+
+  // Calculates the average total points of the students stored in the collection
+  public double getAverage()  {
+
+  }
+
+  // Calculates the maximum total points of the students stored in the collection
+  public int getMax() {
+
+  }
+
+  // Calculates the minimum total points of the students stored in the collection
+  public int getMin() {
+
+  }
+
+  // Calculates the median total points of the students stored in the collection
+  public int getMedian()  {
+
+  }
+
+  // Sorts the collection on a student’s full name using the Insertion Sort algorithm
+  public int insertionSort() {
+
+  }
+
+  // Tests whether the collection is empty
+  public bool isEmpty() {
+
+  }
+
+  // Searches the collection using the Linear Search algorithm
+  public int linearSearch()  {
+
+  }
+
+  // Removes a Student object from the collection
+  public double remove()  {
+
+  }
+
+  // Sorts the collection on a student’s full name using the Selection Sort algorithm
+  public int selectionSort() {
+
+  }
+
+  // Sorts the collection on a student’s total points using the Selection Sort algorithm
+  public int sortArray()  {
+
+  }
+
 }
