8457326187785f2835fa9024bc10f038af9515c4
willmcross <hatchinmorenos@hotmail.com>
2015-01-31 17:54:10 -0500

    Update Driver.java
    Added in file reader. Beginning creation of collector object

--- Project 1/src/Driver.java
+++ Project 1/src/Driver.java
@@ -3,4 +3,45 @@ public class Driver {
 
   // Declarations and Runtime Constants
 
-}
+        public void execute() throws IOException {
+           ArrayList<String> list = new ArrayList<>();
+
+	// New BufferedReader.
+	BufferedReader reader = new BufferedReader(new FileReader(
+		"C:\\Contest.Input.A.txt"));
+
+       
+        
+	// Add all lines from file to ArrayList.
+	while (true) {
+	    String line = reader.readLine();
+	    if (line == null) {
+		break;
+	    }
+	    list.add(line);
+	}
+
+	// Close it.
+	reader.close();
+
+	
+            for(String line : list) {
+
+                // splits each line into a token
+                StringTokenizer st = new StringTokenizer(line);
+                String info = st.nextToken();
+           
+                // Takes each line, creates a series of variables from the substrings. Values correlate to Contest.Description. 
+                String studentFirstName = info.substring(0,9).trim();
+                String studentLastName =  info.substring(9,18).trim();
+                String finalPosition = info.substring(19, 20).trim();
+                int projectAttempted = Integer.parseInt(info.substring(21,22).trim());
+                int projectCompleted = Integer.parseInt(info.substring(23).trim());
+                int totalPoints = Integer.parseInt(info.substring(24,27).trim());
+     
+               
+            }
+            
+            
+            
+    }
