84b93bddf45b6d6f0a7f73f41777a13ce28a247c
James Spinella <james.spinella@jatsby.com>
2015-02-01 17:17:24 -0500

    Made some syntax fixes!

--- Project 1/src/Collection.java
+++ Project 1/src/Collection.java
@@ -1,27 +1,29 @@
+package Project1;
+
 
 public class Collection {
-    
-  private int studentElement;
-  private int max;
-  
-  
-  
-  // Declare n-element Array
-  private Student[] array = new Student[max];
 
-  
-  
-  // Adds a Student object to the collection
-  public void add(Student newStudent) {
-      array[studentElement++] = newStudent;
-  }
-  
-  
-// Searches the collection using the Binary Search algorithm
+    private int studentElement;
+    private int max;
+
+
+
+    // Declare n-element studentArray
+    private Student[] studentArray = new Student[max];
+
+
+
+    // Adds a Student object to the collection
+    public void add(Student newStudent) {
+        studentArray[studentElement++] = newStudent;
+    }
+
+
+    // Searches the collection using the Binary Search algorithm
     public int binarySearch(int value) {
         int index = -1;
         int lower = 0;
-        int upper = array.length - 1;
+        int upper = studentArray.length - 1;
         int current;
 
         while (lower <= upper && index == -1) {
@@ -29,7 +31,7 @@ public class Collection {
             if ([current] == value) {
                 index = current;
             } else {
-                if (array[current] < value) {
+                if (studentArray[current] < value) {
                     lower = current + 1;
                 } else {
                     upper = current - 1;
@@ -45,7 +47,7 @@ public class Collection {
     public int bubbleSort()  {
         for(int y = 0; y < numElems;  y++){
             for(int x = 0; x < numElems - 1; x++) {
-                if(array[x] > array[x + 1]){
+                if(studentArray[x] > studentArray[x + 1]){
                     swap(x, x + 1);
                     bubbleSortCopyCount++;
                 }
@@ -56,9 +58,9 @@ public class Collection {
     }
 
     public void swap(int pos1, int pos2) {
-        int temp = array[pos1];
-        array[pos1] = array[pos2];
-        array[pos2] = temp;
+        int temp = studentArray[pos1];
+        studentArray[pos1] = studentArray[pos2];
+        studentArray[pos2] = temp;
     }
 
 
@@ -77,10 +79,10 @@ public class Collection {
     // Calculates the average total points of the students stored in the collection
     public double getAverage()  {
         int sum = 0;
-        for (int i = 0; i < array.length; i++){
-            sum = sum + array[i];
+        for (int i = 0; i < studentArray.length; i++){
+            sum = sum + studentArray[i];
         }
-        double result = sum / array.length;
+        double result = sum / studentArray.length;
         System.out.println(result);
 
         return result;
@@ -90,9 +92,9 @@ public class Collection {
     // Calculates the maximum total points of the students stored in the collection
     public int getMax() {
         int max = 0;
-        for(int n = 0; n < array.length; n++){
-            if(array[n] > max){
-                max = array[n];
+        for(int n = 0; n < studentArray.length; n++){
+            if(studentArray[n] > max){
+                max = studentArray[n];
             }
         }
         return max;
@@ -102,113 +104,114 @@ public class Collection {
     // Calculates the minimum total points of the students stored in the collection
     public int getMin() {
         int min = 0;
-        for (int n = 0; n < array.length; n++) {
-            if (array[n] < min) {
-                min = array[n];
+        for (int n = 0; n < studentArray.length; n++) {
+            if (studentArray[n] < min) {
+                min = studentArray[n];
             }
 
         }
         return min;
     }
 
-  // Calculates the median total points of the students stored in the collection
-  public int getMedian()  {
-      int median = array.length/2
+    // Calculates the median total points of the students stored in the collection
+    public int getMedian()  {
+        int median = studentArray.length/2
 
-      if (array.length % 2 ==0){
-          return median[middle];
-      else{
-          return (median[middle+1] + median[middle])/2;
-          }
-      }
+        if (studentArray.length % 2 ==0){
+            return median[middle];
+            else{
+                return (median[middle+1] + median[middle])/2;
+            }
+        }
 
-  }
+    }
 
-  // Sorts the collection on a student’s full name using the Insertion Sort algorithm
-  public int insertionSort() {
-    int x, temp;
+    // Sorts the collection on a student’s full name using the Insertion Sort algorithm
+    public int insertionSort() {
+        int x, temp;
 
-      for(int y = 1; y < numElems; y++){
-          temp = array[y];
-          x = y;
-          while (x > 0 && array[x-1] >= temp){
-              array[x] = array[x-1];
-              x--;
-              selectionSortCopyCount++;
-          }
-          array[x] = temp;
-      }
-  }
+        for(int y = 1; y < numElems; y++){
+            temp = studentArray[y];
+            x = y;
+            while (x > 0 && studentArray[x-1] >= temp){
+                studentArray[x] = studentArray[x-1];
+                x--;
+                int selectionSortCopyCount = 0;
+                selectionSortCopyCount++;
+            }
+            studentArray[x] = temp;
+        }
+    }
 
-  // Tests whether the collection is empty
-  public bool isEmpty() {
-      if(collection != null && collection.isEmpty()){
-        return true;
-      }
-      else{
-          return false;
-      }
+    // Tests whether the collection is empty
+    public boolean isEmpty() {
+        if(collection != null && collection.isEmpty()){
+            return true;
+        }
+        else{
+            return false;
+        }
 
-  }
+    }
 
 
-  // Searches the collection using the Linear Search algorithm
-  public int linearSearch(int value)  {
-      int index = -1;
+    // Searches the collection using the Linear Search algorithm
+    public int linearSearch(int value)  {
+        int index = -1;
 
-      for(int x = 0; x < array.length && index == -1; x++){
-          if(value == array[x]){
-              index = x;
-          }
-      }
-      return index;
+        for(int x = 0; x < studentArray.length && index == -1; x++){
+            if(value == studentArray[x]){
+                index = x;
+            }
+        }
+        return index;
 
-  }
+    }
 
-  // Removes a Student object from the collection
-  public double remove(array[] item, int index){
-      for (int i = 0; i < array.length; i++){
-          item[i] = "";
-      }
+    // Removes a Student object from the collection
+    public double remove(studentArray[] item, int index){
+        for (int i = 0; i < studentArray.length; i++){
+            item[i] = "";
+        }
 
-  }
+    }
 
-  // Sorts the collection on a student’s full name using the Selection Sort algorithm
-  public int selectionSort() {
-      int min;
+    // Sorts the collection on a student’s full name using the Selection Sort algorithm
+    public int selectionSort() {
+        int min;
 
-      for(int y = 0; y < array.length - 1; y++){
-          min = y;
-          for(int x = y + 1; x < array.length; x++){
-              if(array[x] < array[min]){
-                  min = x;
-              }
-          }
-          if (min > y){
-              swap(y, min);
-          }
-      }
+        for(int y = 0; y < studentArray.length - 1; y++){
+            min = y;
+            for(int x = y + 1; x < studentArray.length; x++){
+                if(studentArray[x] < studentArray[min]){
+                    min = x;
+                }
+            }
+            if (min > y){
+                swap(y, min);
+            }
+        }
 
-  }
+    }
 
-  //look at slide 16 for selection sort
+    //look at slide 16 for selection sort
 
-  // Sorts the collection on a student’s total points using the Selection Sort algorithm
-  public int sortArray()  {
-      int min;
+    // Sorts the collection on a student’s total points using the Selection Sort algorithm
+    public int sortstudentArray()  {
+        int min;
 
-      for(int y = 0; y < array.length - 1; y++){
-          min = y;
-          for(int x =  y + 1; x < array.length; x++){
-              if(array[x] < array[min]){
-                  min = x;
-              }
-          }
-          if (min > y){
-              swap(y, min);
-          }
-      }
+        for(int y = 0; y < studentArray.length - 1; y++){
+            min = y;
+            for(int x =  y + 1; x < studentArray.length; x++){
+                if(studentArray[x] < studentArray[min]){
+                    min = x;
+                }
+            }
+            if (min > y){
+                swap(y, min);
+            }
+        }
 
-  }
+    }
 
 }
--- Project 1/src/Driver.java
+++ Project 1/src/Driver.java
@@ -1,4 +1,4 @@
-
+package Project1;
 
 
 import java.io.*;
@@ -6,8 +6,8 @@ import java.util.*;
 
 public class Driver {
 
-private final int SEARCH_LENGTH;
-private Collection coll;
+    private final int SEARCH_LENGTH;
+    private Collection coll;
 
     public Driver() throws IOException {
         SEARCH_LENGTH = (int)(array.length / 2); // Initialize the runtime constant
@@ -25,129 +25,129 @@ private Collection coll;
     }
 
 
-    
-    
 
-    
-    
+
+
+
+
     public void readInput() throws IOException {
-           ArrayList<String> list = new ArrayList<>();
+        ArrayList<String> list = new ArrayList<String>();
 
-	// New BufferedReader.
-	BufferedReader reader = new BufferedReader(new FileReader(
-		"C:\\Contest.Input.A.txt"));
-
-
-
-	// Add all lines from file to ArrayList.
-	while (true) {
-	    String line = reader.readLine();
-	    if (line == null) {
-		break;
-	    }
-	    list.add(line);
-	}
-
-	// Close it.
-	reader.close();
+        // New BufferedReader
+        BufferedReader reader = new BufferedReader(new FileReader(
+                "C:\\Contest.Input.A.txt"));
 
 
-          
 
-            for(String line : list) {
-                
-                coll.add(new Student(line));
+        // Add all lines from file to ArrayList.
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
             }
-                
+            list.add(line);
+        }
+
+        // Close it.
+        reader.close();
+
+
+
+
+        for(String line : list) {
+
+            coll.add(new Student(line));
+        }
+
 
     }
 
 
-        private void sortCollection () {
-            coll.bubbleSort();
-            coll.selectionSort();
-            coll.insertionSort();
+    private void sortCollection () {
+        coll.bubbleSort();
+        coll.selectionSort();
+        coll.insertionSort();
     }
 
-        
-        public void readInputC() throws IOException {
-           ArrayList<String> list = new ArrayList<>();
 
-	// New BufferedReader.
-	BufferedReader reader = new BufferedReader(new FileReader(
-		"C:\\Contest.Search.txt"));
+    public void readInputC() throws IOException {
+        ArrayList<String> list = new ArrayList<String>();
 
-
-
-	// Add all lines from file to ArrayList.
-	while (true) {
-	    String line = reader.readLine();
-	    if (line == null) {
-		break;
-	    }
-	    list.add(line);
-	}
-
-	// Close it.
-	reader.close();
+        // New BufferedReader.
+        BufferedReader reader = new BufferedReader(new FileReader(
+                "C:\\Contest.Search.txt"));
 
 
-          
 
-            for(String line : list) {
-                
-                coll.add(new Student(line));
+        // Add all lines from file to ArrayList.
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
             }
-                
+            list.add(line);
+        }
+
+        // Close it.
+        reader.close();
+
+
+
+
+        for(String line : list) {
+
+            coll.add(new Student(line));
+        }
+
 
     }
 
-        private void searchCollection () {
-            coll.linearSearch(SEARCH_LENGTH);
-            coll.binarySearch(SEARCH_LENGTH);
-        }
+    private void searchCollection () {
+        coll.linearSearch(SEARCH_LENGTH);
+        coll.binarySearch(SEARCH_LENGTH);
+    }
 
 
+    private void displayReport() {
+        System.out.println("James Spinella, Waseem Baraz, Rachel Bennett, Mike Cross");
+        System.out.println("Project 1\n");
+        // Sort Algorithm Report
+        System.out.println("Sort Algorithm Report:");
+        System.out.println("The Bubble Sort algorithm required " + coll.bubbleSortCopyCount + " copies");
+        System.out.println("The Selection Sort algorithm required " + coll.selectionSortCopyCount + " copies");
+        System.out.println("The Insertion Sort algorithm required " + coll.insertionSortCopyCount + " copies\n");
+        // Records
+        System.out.println("Student Name          Position  Attempted  Completed  Total Points");
+        System.out.println("------------          -------------------  ---------  ------------");
+        System.out.println("\n");
+                // ten records
+                // Statistics Report
+        System.out.println("Statistics Report:");
+        System.out.println("The maximum points earned was" + coll.getMax);
+        System.out.println("The minimum points earned was" + coll.getMin);
+        System.out.println("The average points earned was" + coll.getAverage);
+        System.out.println("The median points earned was" + coll.getMedian + "\n");
+        // Linear Search Results
+        System.out.println("Linear Search Results:");
+        System.out.println("Search String        Found  Not Found  # Probes");
+        System.out.println("-------------        -----  ---------  --------");
+        // print 15 results \n
+        // Binary Search Results
+        System.out.println("Binary Search Results:");
+        System.out.println("Search String        Found  Not Found  # Probes");
+        System.out.println("-------------        -----  ---------  --------");
+        // print 15 results \n
+        // Contents of Contest.Output.txt (do not display on screen)
+        System.out.println("Contents of Contest.Output.txt file:");
+        // print 32 results \n
+    }
 
-        private void displayReport() {
-            System.out.println("James Spinella, Waseem Baraz, Rachel Bennett, Mike Cross");
-            System.out.println("Project 1"\n);
-            // Sort Algorithm Report
-            System.out.println("Sort Algorithm Report:");
-            System.out.println("The Bubble Sort algorithm required " + coll.bubbleSortCopyCount " copies");
-            System.out.println("The Selection Sort algorithm required " + coll.selectionSortCopyCount " copies");
-            System.out.println("The Insertion Sort algorithm required " + coll.insertionSortCopyCount " copies"\n\n);
-            // Records
-            System.out.println("Student Name          Position  Attempted  Completed  Total Points");
-            System.out.println("------------          -------------------  ---------  ------------");
-            System.out.println(
-            // ten records
-            // Statistics Report
-            System.out.println("Statistics Report:");
-            System.out.println("The maximum points earned was" + coll.getMax);
-            System.out.println("The minimum points earned was" + coll.getMin);
-            System.out.println("The average points earned was" + coll.getAverage);
-            System.out.println("The median points earned was" + coll.getMedian));
-            // Linear Search Results
-            System.out.println("Linear Search Results:");
-            System.out.println("Search String        Found  Not Found  # Probes");
-            System.out.println("-------------        -----  ---------  --------");
-            // print 15 results \n
-            // Binary Search Results
-            System.out.println("Binary Search Results:");
-            System.out.println("Search String        Found  Not Found  # Probes");
-            System.out.println("-------------        -----  ---------  --------");
-            // print 15 results \n
-            // Contents of Contest.Output.txt (do not display on screen)
-            System.out.println("Contents of Contest.Output.txt file:");
-            // print 32 results \n
-        }
+    private void calculateStatistics() {
 
-        private void calculateStatistics() {
-
-        }
+    }
 
 
 
 
 }
+
