6c492ec72da6db0803a9682f0eef64cc63d3d286
willmcross <hatchinmorenos@hotmail.com>
2015-02-01 13:08:01 -0500

    Update Student.java

--- Project 1/src/Student.java
+++ Project 1/src/Student.java
@@ -1,3 +1,16 @@
 public class Student {
 
-}
+    
+    public getFullName(){
+        
+    }
+    
+    public getPoints(){
+        
+    }
+    
+    @Override
+     public String toString() {
+    
+         return "["+StudentObject()+"]";
+    }   
