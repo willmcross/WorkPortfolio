b666466a07ce1bd2370b2b64aa803231285a24f4
bennr9 <rachel.bennett16@yahoo.com>
2015-02-16 18:48:14 -0500

    GoFish java

---
+++ Project 2/GoFish.java
@@ -0,0 +1,16 @@
+package Project2;
+/*
+*James Spinella
+*Waseem Baraz
+*Mike Cross
+*Rachel Bennett
+*
+* Go Fish! Project
+*/
+public class GoFish {
+    private Game g = new Game();
+    
+    public static void main (String[] args){
+        g.execute();
+    }
+}
