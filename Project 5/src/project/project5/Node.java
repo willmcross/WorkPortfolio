d8c6724997883aba98ef3499b74a8bd7828f6864
James Spinella <james.spinella@jatsby.com>
2015-04-25 13:33:37 -0400

    Nodey Node

--- Project 5/src/project/project5/Node.java
+++ Project 5/src/project/project5/Node.java
@@ -5,4 +5,31 @@ package project.project5;
  */
 
 public class Node {
+    
+    private State state;
+    private Node next;
+    
+    /**
+     * Returns the node to the right of the current node.
+     * @return the node to the right of the current node.
+     */
+    public Node getNext() {
+       return next; 
+    }
+
+    /**
+     * Returns the state object stored in the current node.
+     * @return the state object stored in the current node.
+     */
+    public State getState() {
+        return state;
+    }
+
+    /**
+     * Sets the next pointer to the node to the right of the current node.
+     * @param node the node to assign to the next pointer.
+     */
+    public void setNext(Node node) {
+        next = node;
+    }
 }
