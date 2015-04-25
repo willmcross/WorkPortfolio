1b996797d3340e21ed3977615b385ce36506dcc5
bennr9 <rachel.bennett16@yahoo.com>
2015-04-25 01:21:11 -0400

    1/10 of Driver Probably

--- Project 5/src/project/project5/Driver.java
+++ Project 5/src/project/project5/Driver.java
@@ -1,5 +1,9 @@
 package project.project5;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
 /**
  * Project.java - Executes driver, etc.
  */
@@ -8,7 +12,22 @@ public class Driver {
     
     void execute(String[] args) {
 
+        HashTable HT = new HashTable();
+        State state = new State();
+        String[] stateName = new String[9];
         
-    
+        try{
+                InputStream input = this.getClass().getResourceAsStream("States.Input.txt");
+                InputStreamReader isr = new InputStreamReader(input);
+                BufferedReader reader = new BufferedReader(isr);
+                
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    String[] split = line.split(",|\\ ");
+                }
+        }
+            catch(IOException e){
+                System.out.println("An error has occured.");
+            }
     }
 }
